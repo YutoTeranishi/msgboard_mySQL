41ba385e6d631836e7ae2941fedabca366712096
Yuto Teranishi <yt05271992@yahoo.co.jp>
2022-02-22 18:06:34 +0900

    掲示板アプリ
    完成形

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'msgboard/index'
+  post 'msgboard/index'
+  get 'msgboard', to: 'msgboard#index'
+  post 'msgboard', to: 'msgboard#index'
 end
